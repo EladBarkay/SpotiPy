1ff9e80f0d84f0936d72ebb84f8ae42304bb2d8a
EladBarkay <eladb1231@gmail.com>
2022-02-09 17:20:10 +0200

    minor changes

--- helper/write_them_all.py
+++ helper/write_them_all.py
@@ -4,18 +4,21 @@ from os.path import isfile, join
 import objects.song as song
 
 
-def get_songs():
+def get_songs(songs_folder_path="C:\\Users\\User\\Desktop\\learn process\\2022-02\\9.2\\SpotiPy\\Resources\\songs"):
     songs_list: typing.List[song.Song] = []
-    songs_folder_path = "C:\\Users\\User\\Desktop\\learn process\\2022-02\\9.2\\SpotiPy\\Resources\\songs"
-    song_files_paths = [join(songs_folder_path, f) for f in listdir(songs_folder_path) if isfile(join(songs_folder_path, f))]
+    song_files_paths = [join(songs_folder_path, f) for f in listdir(songs_folder_path) if
+                        isfile(join(songs_folder_path, f))]
     try:
         for song_path in song_files_paths:
-
             songs_list.append(song.load(song_path))
     except Exception as e:
         print(e)
 
     return songs_list
+
+
+def write_items(songs_path):
+    songs = get_songs(songs_path)
 
 
 print(get_songs())
