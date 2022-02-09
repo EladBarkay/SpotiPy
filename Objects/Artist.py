089d9adf92570ff2cbeb557f5b60e4e9d82e4bda
EladBarkay <eladb1231@gmail.com>
2022-02-09 11:27:29 +0200

    add genre to artist

--- Objects/Artist.py
+++ Objects/Artist.py
@@ -7,6 +7,7 @@ class Artist(User):
     albums: typing.List[str]
     genre: str
 
-    def __init__(self, _id: str, name: str, playlists: typing.Dict[str, str], albums: typing.List[str]):
+    def __init__(self, _id: str, name: str, playlists: typing.Dict[str, str], albums: typing.List[str], genre: str):
         super().__init__(_id, name, True, playlists)
         self.albums = albums
+        self.genre = genre
