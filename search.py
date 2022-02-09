0d68d5f47021adf9ca4df8ef36fa1900eddcc581
EladBarkay <eladb1231@gmail.com>
2022-02-09 18:21:54 +0200

    new all_artists_func, with 'main' for tests

--- search.py
+++ search.py
@@ -1,12 +1,23 @@
 import typing
-import objects.song as song
+import objects.song as Song
+from objects.spotipy_generic_obj import SpotipyGenericObj
 from helper.write_them_all import get_songs
 
 
 class Searcher:
-    songs: typing.List[song.Song]
+    songs: typing.List[Song.Song]
 
     def __init__(self):
         self.songs = get_songs()
 
-    
+    def all_artists(self):
+        artists: typing.List[SpotipyGenericObj] = []
+        for song in self.songs:
+            for artist in song.artists:
+                if artist not in artists:
+                    artists.append(artist)
+        return artists
+
+
+x = Searcher()
+print(x.all_artists())
