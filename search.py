88587a2070f846bcb1c1f9e96b62f913d0946395
EladBarkay <eladb1231@gmail.com>
2022-02-09 19:18:43 +0200

    func for all songs of an artist, and a func for artist top X songs

--- search.py
+++ search.py
@@ -1,4 +1,7 @@
+import operator
 import typing
+
+import consts.general
 import objects.song as Song
 from objects.spotipy_generic_obj import SpotipyGenericObj
 from helper.write_them_all import get_songs
@@ -26,7 +29,18 @@ class Searcher:
                     albums.append(song.album)
         return albums
 
+    def all_artist_songs(self, artist_id: str):
+        artist_songs: typing.List[Song.Song] = []
+        for song in self.songs:
+            if artist_id in [artist.id for artist in song.artists]:
+                artist_songs.append(song)
+        return sorted(artist_songs, key=operator.attrgetter(consts.general.POPULARITY), reverse=True)
+
+    def artist_top_songs(self, artist_id: str, countToShow=10):
+        all_songs = self.all_artist_songs(artist_id)
+        return all_songs[:countToShow]
+
 
 x = Searcher()
-for i in x.artist_albums("3MZsBdqDrRTJihTHQrO6Dq"):
+for i in x.artist_top_songs("3MZsBdqDrRTJihTHQrO6Dq"):
     print(i.__dict__)
