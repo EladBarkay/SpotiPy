dff115150ffa22d64d626f64d3b6083bf0c8ca51
EladBarkay <eladb1231@gmail.com>
2022-02-10 08:58:52 +0200

    add_song and remove song functions

--- objects/playlist.py
+++ objects/playlist.py
@@ -1,5 +1,6 @@
 import typing
 from objects.spotipy_generic_obj import SpotipyGenericObj
+import consts.custom_exceptions as ex
 
 
 class Playlist(SpotipyGenericObj):
@@ -8,3 +9,13 @@ class Playlist(SpotipyGenericObj):
     def __init__(self, _id: str, name: str, songs: typing.List[str]):
         super().__init__(_id, name)
         self.songs = songs
+
+    def add_song(self, song_id: str):
+        if song_id in self.songs:
+            raise ex.PlaylistAlreadyContainsThisSong()
+        self.songs.append(song_id)
+
+    def remove_song(self, song_id: str):
+        if song_id not in self.songs:
+            raise ex.PlaylistDoNotContainThisSong()
+        self.songs.remove(song_id)
