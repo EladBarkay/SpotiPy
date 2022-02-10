fc9051ef447c995a24aa271170af85e4fc755f17
EladBarkay <eladb1231@gmail.com>
2022-02-10 08:57:32 +0200

    Some playlist exeptions and free user limitation exception

--- consts/custom_exceptions.py
+++ consts/custom_exceptions.py
@@ -1,4 +1,23 @@
 class PlaylistNameAlreadyExistsError(Exception):
-
     def __init__(self, msg: str = "Playlist name already exists in this users library"):
+        super().__init__(msg)
+
+
+class InvalidFreeUserOperation(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class PlaylistAlreadyContainsThisSong(Exception):
+    def __init__(self, msg: str = None):
+        super().__init__(msg)
+
+
+class PlaylistDoNotContainThisSong(Exception):
+    def __init__(self, msg: str = None):
+        super().__init__(msg)
+
+
+class PlaylistNotExistsError(Exception):
+    def __init__(self, msg: str = None):
         super().__init__(msg)
