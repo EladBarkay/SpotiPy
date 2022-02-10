a14402fc2057891537a6dd9a0d8c00323180c48a
EladBarkay <eladb1231@gmail.com>
2022-02-10 09:32:32 +0200

    new get function: gets func name and kwords, and return the REAL function ans

--- search.py
+++ search.py
@@ -5,6 +5,7 @@ import consts.general
 import objects.song as Song
 from objects.spotipy_generic_obj import SpotipyGenericObj
 from helper.write_them_all import get_songs
+import consts.custom_exceptions as ex
 
 
 class Searcher:
@@ -13,7 +14,7 @@ class Searcher:
     def __init__(self):
         self.songs = get_songs()
 
-    def all_artists(self):
+    def artists(self):
         artists: typing.List[SpotipyGenericObj] = []
         for song in self.songs:
             for artist in song.artists:
@@ -29,11 +30,11 @@ class Searcher:
                     albums.append(song.album)
         return albums
 
-    def all_artist_songs(self, artist_id: str):
+    def artist_songs(self, artist_id: str):
         return [song for song in self.songs if artist_id in [artist.id for artist in song.artists]]
 
     def artist_top_songs(self, artist_id: str, countToShow=10):
-        all_songs = self.all_artist_songs(artist_id)
+        all_songs = self.artist_songs(artist_id)
         all_songs.sort(key=operator.attrgetter(consts.general.POPULARITY), reverse=True)
         # git commit -m "func for all songs of an artist, and a func for artist to"
         return all_songs[:countToShow]
@@ -41,7 +42,8 @@ class Searcher:
     def album_songs(self, album_id):
         return [song for song in self.songs if song.album.id == album_id]
 
-
-x = Searcher()
-for i in x.artist_top_songs("39jFFncu6W0phhYK16Dp9g"):
-    print(i.__dict__)
+    def get(self, search_type, **search_params):
+        func = getattr(self, search_type)
+        if func is None:
+            raise ex.InvalidSearchOption(f"search module has no {search_type} option")
+        return func(**search_params)
