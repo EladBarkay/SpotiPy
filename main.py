2bf24bf9df62e6c01d97ce8a70bc881579bb4ed8
EladBarkay <eladb1231@gmail.com>
2022-02-10 11:47:07 +0200

    test main

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import objects.artist
+import objects.user
+import search
+
+
 def main():
-    pass
+    s = search.Searcher()
+    artists = s.artists()
+    for a in artists:
+        user = objects.artist.Artist(a.id, a.name)
+        user.albums = s.artist_albums(artist_id=a.id)
+        user.save()
+
+    artist = objects.user.load("Avihu Pinchasov Rhythm Club")
+    print(artist)
 
 
 if __name__ == '__main__':
