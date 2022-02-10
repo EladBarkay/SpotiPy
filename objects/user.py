a66626fb16f2204e2f1f0538ede3c4b22a2c98d9
EladBarkay <eladb1231@gmail.com>
2022-02-10 11:09:27 +0200

    easier construction for user, and fix path func

--- objects/user.py
+++ objects/user.py
@@ -4,20 +4,21 @@ import consts.custom_exceptions as ex
 import search
 from objects.playlist import Playlist
 from file_managment import save_and_load as manager
-from spotipy_generic_obj import SpotipyGenericObj
+from objects.spotipy_generic_obj import SpotipyGenericObj
 
 
 class User(SpotipyGenericObj):
     is_premium: bool
     playlists: typing.List[Playlist]  # playlist id to Playlist
 
-    def __init__(self, _id: str, name: str, is_premium: bool, playlists: typing.List[Playlist]):
+    def __init__(self, _id: str, name: str, is_premium: bool = False, playlists: typing.List[Playlist] = None):
         super().__init__(_id, name)
         self.is_premium = is_premium
-        self.playlists = playlists
+        self.playlists = playlists if playlists is not None else []
 
     def _object_path(self):
-        return f'{consts.FILES_ROOT_PATH}\\{consts.SONGS}\\{consts.SONG_FILE_START}{self.id}{consts.JSON_FILE_TYPE}'
+        return f'{consts.FILES_ROOT_PATH}\\{consts.USERS}\\' \
+               f'{consts.USER_FILE_START}{self.name}{consts.PICKLE_FILE_TYPE}'
 
     def save(self):
         manager.save(self, self._object_path())
